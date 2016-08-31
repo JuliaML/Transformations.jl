0368322945fb04ceec529a5e9095fa04e31c8dfa
Thomas Breloff <tom@breloff.com>
2016-08-30 23:23:24 -0400

    working on affine

--- src/Transformations.jl
+++ src/Transformations.jl
@@ -10,17 +10,31 @@ import Base: rand
 import LearnBase: transform, transform!, learn, learn!
 import StatsBase: logistic, logit
 
+function zero!{T,N}(v::AbstractArray{T,N})
+    for i in eachindex(v)
+        v[i] = zero(T)
+    end
+end
+
 # ----------------------------------------------------------------
 
 # part of a subgraph
 immutable Node{TYPE,T}
-    v::T  # value
-    δ::T  # sensitivity
+    val::T  # value
+    # δ::T  # sensitivity
     ∇::T  # gradient
 end
 
+function Node{T}(nodetype::Symbol, val::T) #, δ::T = zeros(val))
+    Node{nodetype, T}(val, zeros(val))
+end
+
+Base.show{TYPE}(io::IO, node::Node{TYPE}) = print(io, "$TYPE$(size(node.val))")
+
 # y = wx + b
 immutable Affine{V,M} <: Transformation
+    nin::Int
+    nout::Int
     x::Node{:input, V}
     w::Node{:param, M}
     b::Node{:param, V}
@@ -28,20 +42,51 @@ immutable Affine{V,M} <: Transformation
 end
 
 function Affine{T}(::Type{T}, nin::Int, nout::Int)
-    Affine{T,N}(
-        Node{:input}(zeros(T, nin)),
-        Node{:param}(zeros(T, nout, nin)),
-        Node{:param}(zeros(T, nout)),
-        Node{:output}(zeros(T, nout))
-    )
+    x = Node(:input, zeros(T, nin)),
+    w = Node(:param, zeros(T, nout, nin)),
+    b = Node(:param, zeros(T, nin)), #, ones(T, nin)), # sensitivity of b is constant
+    y = Node(:output, zeros(T, nin)) #, ones(T, nin))
+    Affine(nin,nout,x,w,b,y)
 end
 
+Base.show(io::IO, t::Affine) = print(io, "Affine{$(t.nin)-->$(t.nout), x=$(t.x), w=$(t.w), b=$(t.b), y=$(t.y)}")
+
+# copy x and compute y
+function transform!(aff::Affine, x::AbstractVector)
+    copy!(aff.x.val, x)
+    copy!(aff.y.val, aff.b.val)
+    for i=1:aff.nout
+        aff.y.val[i] += sum(aff.w.val[i,j] * x[j] for j=1:aff.nin)
+    end
+end
+
+# update the partial derivatives:
+#   ∇x = ∂L/∂x
+#   ∇w = ∂L/∂w
+#   ∇b = ∂L/∂b
+# use the chain rule, assuming that we've already updated ∇y = ∂L/∂y
+function grad!(aff::Affine)
+    # ∇x, ∇w
+    for i=1:aff.nin
+        ∇xᵢ = zero(eltype(aff.x.∇))
+        for o=1:aff.nout
+            ∇xᵢ += aff.w.val[o,i] * aff.y.∇[o]
+            aff.w.∇[o,i] = aff.x.val[i] * aff.y.∇[o]
+        end
+        aff.x.∇[i] = ∇xᵢ
+    end
+
+    # ∇b
+    copy!(aff.b.∇, aff.y.val)
+    return
+end
 
 # ----------------------------------------------------------------
 
-# We will be using the AutoGrad package for automatic differentiation of our
-# transform methods
-
+# TODO:
+#   - tests for Affine
+#   - implement some basic activations: sigmoid (logistic), tanh, relu
+#   - simple Chainer to connect transformations (graphs can be later: general DAGs at first, then allow cycles)
 
 
 # ----------------------------------------------------------------
