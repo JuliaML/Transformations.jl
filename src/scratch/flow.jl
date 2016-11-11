457e8b469fb94169feda3c5afc8bf1ee1d51a862
Tom Breloff <tom@breloff.com>
2016-11-11 17:44:09 -0500

    flow

--- src/scratch/flow.jl
+++ src/scratch/flow.jl
@@ -3,34 +3,52 @@
 # This is experimental, and currently unused
 # ---------------------------------------------------------------------------
 
-@reexport using Flow
+using DataFlow
 
 export
 	edges
 
-function edges(v::DVertex, source = Int[], destiny = Int[], names = Symbol[], cache = Flow.ODict())
-	haskey(cache,v) && return source,destiny,names
-	cache[v] = length(cache) + 1
-	push!(names, string(v.value))
-	for v′ in inputs(v)
+function nodename(v)
+    val = v.value
+    isconst = isa(val, DataFlow.Constant)
+    val = isconst ? val.value : val
+    val, isconst
+end
+
+
+function edges(v::DVertex, source = Int[], destiny = Int[], names = Symbol[], cache = DataFlow.ODict())
+	# haskey(cache,v) && return source,destiny,names
+    name, isconst = nodename(v)
+    haskey(cache, isconst ? name : v) && return source,destiny,names
+    push!(names, name)
+    didx = length(cache)+1
+    cache[isconst ? name : v] = didx
+    # @show name cache
+	# cache[v] = length(cache) + 1
+    # val = v.value
+	# push!(names, string(isa(val, DataFlow.Constant) ? val.value : val))
+	for v′ in DataFlow.inputs(v)
 		edges(v′, source, destiny, names, cache)
-		push!(source, cache[v′])
-		push!(destiny, cache[v])
+		# push!(source, cache[v′])
+		# push!(destiny, cache[v])
+        sname, sisconst = nodename(v′)
+		push!(source, cache[sisconst ? sname : v′])
+		push!(destiny, didx)
 	end
 	source, destiny, names
 end
-edges(g::Flow.SyntaxGraph, args...) = edges(g.output, args...)
+edges(g::DataFlow.SyntaxGraph, args...) = edges(g.output, args...)
 
 using PlotRecipes
-@recipe function f(g::Flow.SyntaxGraph)
-	source, destiny, names = edges(g.output)
-    arrow --> arrow()
-    markersize --> 50
-    markeralpha --> 0.2
-    linealpha --> 0.4
-    linewidth --> 2
-    names --> names
-    func --> :tree
+@recipe function f(g::Union{DataFlow.SyntaxGraph,DVertex})
+	source, destiny, names = edges(g)
+    # arrow --> arrow()
+    # markersize --> 50
+    # markeralpha --> 0.2
+    # linealpha --> 0.4
+    # linewidth --> 2
+    names --> map(string, names)
+    method --> :tree
     root --> :left
     PlotRecipes.GraphPlot((source, destiny))
 end
