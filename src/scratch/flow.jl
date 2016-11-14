544201fda841332c4d1f844ca80071b498c66007
Tom Breloff <tom@breloff.com>
2016-11-14 13:30:22 -0500

    cleanup

--- src/scratch/flow.jl
+++ src/scratch/flow.jl
@@ -17,20 +17,13 @@ end
 
 
 function edges(v::DVertex, source = Int[], destiny = Int[], names = Symbol[], cache = DataFlow.ODict())
-	# haskey(cache,v) && return source,destiny,names
     name, isconst = nodename(v)
     haskey(cache, isconst ? name : v) && return source,destiny,names
     push!(names, name)
     didx = length(cache)+1
     cache[isconst ? name : v] = didx
-    # @show name cache
-	# cache[v] = length(cache) + 1
-    # val = v.value
-	# push!(names, string(isa(val, DataFlow.Constant) ? val.value : val))
 	for v′ in DataFlow.inputs(v)
 		edges(v′, source, destiny, names, cache)
-		# push!(source, cache[v′])
-		# push!(destiny, cache[v])
         sname, sisconst = nodename(v′)
 		push!(source, cache[sisconst ? sname : v′])
 		push!(destiny, didx)
@@ -42,11 +35,6 @@ edges(g::DataFlow.SyntaxGraph, args...) = edges(g.output, args...)
 using PlotRecipes
 @recipe function f(g::Union{DataFlow.SyntaxGraph,DVertex})
 	source, destiny, names = edges(g)
-    # arrow --> arrow()
-    # markersize --> 50
-    # markeralpha --> 0.2
-    # linealpha --> 0.4
-    # linewidth --> 2
     names --> map(string, names)
     method --> :tree
     root --> :left
